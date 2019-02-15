1563c33ae5a3535107de9ab57582886c6c45842b
Jungea <jungea2241@gmail.com>
2019-02-15 23:01:14 +0900

    hw6_4 상담예약프로그램 db 테이블 작성

--- resDB.sql
+++ resDB.sql
@@ -69,6 +69,65 @@ LOCK TABLES `professor` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `resdescript`
+--
+
+DROP TABLE IF EXISTS `resdescript`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `resdescript` (
+  `desID` int(5) NOT NULL AUTO_INCREMENT,
+  `resID` int(5) NOT NULL,
+  `startTime` date NOT NULL,
+  `content` varchar(20) NOT NULL,
+  PRIMARY KEY (`desID`),
+  KEY `resID` (`resID`),
+  CONSTRAINT `resdescript_ibfk_1` FOREIGN KEY (`resID`) REFERENCES `reservation` (`resid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `resdescript`
+--
+
+LOCK TABLES `resdescript` WRITE;
+/*!40000 ALTER TABLE `resdescript` DISABLE KEYS */;
+/*!40000 ALTER TABLE `resdescript` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `reservation` (
+  `resID` int(5) NOT NULL AUTO_INCREMENT,
+  `resTime` date NOT NULL,
+  `approve` int(1) NOT NULL DEFAULT '0',
+  `cancel` int(1) NOT NULL DEFAULT '0',
+  `resTitle` varchar(20) NOT NULL,
+  `stdID` char(9) NOT NULL,
+  `proID` char(9) NOT NULL,
+  PRIMARY KEY (`resID`),
+  KEY `stdID` (`stdID`),
+  KEY `proID` (`proID`),
+  CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`stdID`) REFERENCES `student` (`stdid`),
+  CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`proID`) REFERENCES `professor` (`proid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservation`
+--
+
+LOCK TABLES `reservation` WRITE;
+/*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `student`
 --
 
@@ -105,4 +164,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-02-15 22:26:11
+-- Dump completed on 2019-02-15 22:58:18
